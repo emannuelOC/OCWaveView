7da03c0f5b147fd69060392c8b117b6957ff3866
Emannuel Carvalho <emannuel.carvalho@icloud.com>
2017-03-15 19:57:18 -0300

    initial commit

---
+++ OCWaveView/OCWaveView.h
@@ -0,0 +1,19 @@
+//
+//  OCWaveView.h
+//  OCWaveView
+//
+//  Created by Emannuel Carvalho on 15/03/17.
+//  Copyright © 2017 OC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OCWaveView.
+FOUNDATION_EXPORT double OCWaveViewVersionNumber;
+
+//! Project version string for OCWaveView.
+FOUNDATION_EXPORT const unsigned char OCWaveViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCWaveView/PublicHeader.h>
+
+
